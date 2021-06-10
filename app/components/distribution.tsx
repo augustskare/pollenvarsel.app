b4aeb61d2922e983bb8dab2a239ac176a99c7b51
August Skare <post@augustskare.no>
2021-06-10 07:57:16 +0200

    Region view

---
+++ app/components/distribution.tsx
@@ -0,0 +1,48 @@
+const SIZE = {
+  cx: 13,
+  cy: 13,
+};
+
+function Distribution(props) {
+  let classNames = 'symbol';
+  if (props.large) {
+    classNames += ' large';
+  }
+
+  return (
+    <svg
+      className={classNames}
+      viewBox="0 0 26 26"
+      fill="none"
+      aria-hidden="true"
+      data-dist={props.dist}
+    >
+      <circle
+        fill="currentColor"
+        r="4"
+        {...SIZE}
+        style={{ fill: 'var(--color-dist1, currentColor)' }}
+      />
+      <circle
+        stroke="currentColor"
+        r="6.5"
+        {...SIZE}
+        style={{ stroke: 'var(--color-dist2, currentColor)' }}
+      />
+      <circle
+        stroke="currentColor"
+        r="9.5"
+        {...SIZE}
+        style={{ stroke: 'var(--color-dist3, currentColor)' }}
+      />
+      <circle
+        stroke="currentColor"
+        r="12.5"
+        {...SIZE}
+        style={{ stroke: 'var(--color-dist4, currentColor)' }}
+      />
+    </svg>
+  );
+}
+
+export { Distribution };
--- app/routes/$region.tsx
+++ app/routes/$region.tsx
@@ -1,5 +1,90 @@
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs';
+import type { LinksFunction, LoaderFunction } from 'remix';
+import { useRouteData } from 'remix';
+import parser from 'fast-xml-parser';
+
+import { normalizeForecast, lowerCaseObjectKeys } from '../forecast';
+import { Distribution } from '../components/distribution';
+import styles from '../styles/region.css';
+
+export let links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
+
+export let loader: LoaderFunction = async ({ params }) => {
+  const resp = await fetch(
+    `http://xml.pollenvarslingen.no/pollenvarsel.asmx/GetAllRegions?userKey=${process.env.POLLENVARSEL_API_KEY}`
+  );
+  const xml = await resp.text();
+  const data = parser.parse(xml).RegionForecast.Days.RegionDay;
+
+  const forecast = normalizeForecast(
+    data.map((day) => {
+      return {
+        date: day.Date,
+        regions: day.Regions.Region.map((region) => {
+          return {
+            ...lowerCaseObjectKeys(region),
+            pollen: region.PollenTypes.Pollen.map(lowerCaseObjectKeys),
+          };
+        }),
+      };
+    })
+  );
+
+  return forecast.find((f) => f.slug === params.region);
+};
+
 function Region() {
-  return <p>Region</p>;
+  const region = useRouteData();
+
+  return (
+    <>
+      <Tabs className="content section">
+        <TabList className="tabs">
+          {region.forecast.map((forecast) => {
+            return (
+              <Tab key={`tab-${forecast.date}`} className="tab">
+                {forecast.date}
+              </Tab>
+            );
+          })}
+        </TabList>
+
+        <TabPanels>
+          {region.forecast.map((forecast) => {
+            return (
+              <TabPanel key={`panel-${forecast.date}`}>
+                <p className="description">{forecast.description}</p>
+
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th className="heading table-cell">Type</th>
+                      <th className="heading table-cell">Spredning</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {forecast.pollen.map((pollen) => {
+                      return (
+                        <tr key={pollen.id} className="table-row">
+                          <td className="table-cell">{pollen.name}</td>
+                          <td className="distribution table-cell">
+                            <Distribution dist={pollen.distribution} />
+                            {pollen.description}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </TabPanel>
+            );
+          })}
+        </TabPanels>
+      </Tabs>
+    </>
+  );
 }
 
 export default Region;
