3fb173ef1331c2d7b27e0a93a7a8d1888da9673c
August Skare <post@augustskare.no>
2021-06-21 13:42:22 +0200

    Only load .env in dev mode

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,9 +1,6 @@
-import ReactDOMServer from 'react-dom/server';
-import type { EntryContext } from 'remix';
-import { RemixServer } from 'remix';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import ReactDOMServer from "react-dom/server";
+import type { EntryContext } from "remix";
+import { RemixServer } from "remix";
 
 export default function handleRequest(
   request: Request,
@@ -15,11 +12,11 @@ export default function handleRequest(
     <RemixServer context={remixContext} url={request.url} />
   );
 
-  return new Response('<!DOCTYPE html>' + markup, {
+  return new Response("<!DOCTYPE html>" + markup, {
     status: responseStatusCode,
     headers: {
       ...Object.fromEntries(responseHeaders),
-      'Content-Type': 'text/html',
+      "Content-Type": "text/html",
     },
   });
 }
